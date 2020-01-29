ba1554b0d08f4a2492290418686e50e5bb6604b0
maoan <9dzy@163.com>
2020-01-29 21:58:17 +0800

    first

--- src/components/zshdlist.vue
+++ src/components/zshdlist.vue
@@ -156,7 +156,7 @@
 
     .xdzs-media-box__info {
         margin-top: 16px;
-        padding-bottom: 4px;
+        padding-bottom: 5px;
         font-size: 13px;
         color: #CECECE;
         line-height: 1em;
